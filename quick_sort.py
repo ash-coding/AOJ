f3a60864aa1691d5c6b2494dabee999ac279e6d6
ash-coding <64770032+ash-coding@users.noreply.github.com>
2021-03-20 15:31:02 +0900

    update:add comment

--- quick_sort.py
+++ quick_sort.py
@@ -55,3 +55,30 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+"""
+def partition(A, p, r):
+    x = A[r][1]
+    i = p - 1
+    for j in range(p, r):
+        if A[j][1] <= x:
+            i += 1
+            A[i], A[j] = A[j], A[i]
+    A[i + 1], A[r] = A[r], A[i + 1]
+    return i + 1
+
+def quick_sort(A, p, r):
+    if p < r:
+        q = partition(A, p, r)
+        quick_sort(A, p, q - 1)
+        quick_sort(A, q + 1, r)
+
+n = int(input())
+A = []
+for _ in range(n):
+    c, v = map(str, input().split())
+    A.append([c, int(v)])
+quick_sort(A, 0, n - 1)
+for e in A:
+    print(e[0] + ' ' + str(e[1]))
+"""
