2fe5863cdaac3422ef0a706e484b11234b9221c1
ash-coding <64770032+ash-coding@users.noreply.github.com>
2021-03-22 22:45:54 +0900

    update: fix print format

--- maximum_heap.py
+++ maximum_heap.py
@@ -2,8 +2,8 @@ import sys
 import io
 
 _INPUT = """\
-10
-4 1 3 2 16 9 10 14 8 7
+12
+8 6 11 4 1 9 12 5 7 3 2 10
 """
 sys.stdin = io.StringIO(_INPUT)
 
@@ -14,10 +14,6 @@ H = list(map(int, input().split()))
 
 H = list(map(lambda x: x * (-1), H))
 heapq.heapify(H)
-
-ans = [0] * n
-for i in range(n):
-    ans[i] = heapq.heappop(H) * (-1)
-
+H = list(map(lambda x: x * (-1), H))
 print(' ', end = '')
-print(*ans)
+print(*H)
