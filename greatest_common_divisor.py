84328f0c14703e40f81ae8f220a4535bacfca18e
ash-coding <64770032+ash-coding@users.noreply.github.com>
2021-03-28 22:08:30 +0900

    add: great common divisor

---
+++ greatest_common_divisor.py
@@ -0,0 +1,24 @@
+import sys
+import io
+
+_INPUT = """\
+5 3
+"""
+sys.stdin = io.StringIO(_INPUT)
+
+def gcd(x, y):
+    if x < y:
+        x, y = y, x
+    while y > 0:
+        r = x % y
+        x = y
+        y = r
+    return x
+
+def main():
+    x, y = map(int, input().split())
+    ans = gcd(x, y)
+    print(ans)
+
+if __name__ == '__main__':
+    main()
